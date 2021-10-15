6212962c5121630fe06e7e0adefb780f0989b43e
Ruslan Agaev <ruslanagaev@list.ru>
2021-10-15 17:59:01 +0300

    FileServer_v1

--- spring-java-config/src/main/kotlin/ru/sber/config/ServicesConfig.kt
+++ spring-java-config/src/main/kotlin/ru/sber/config/ServicesConfig.kt
@@ -1,23 +1,14 @@
 package ru.sber.config
 
-import org.springframework.context.annotation.Bean
+
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import ru.sber.services.FirstService
+
 
 @Configuration
-@ComponentScan
-class ServicesConfig {
-    @Bean
-    fun service(): FirstService {
-        return FirstService()
-    }
-
-    @Bean
-    fun secondService() {
-    }
-}
+@ComponentScan("ru.sber.services")
+class ServicesConfig
 
 @Configuration
-@ComponentScan("ru.sber.anotherservices")
+@ComponentScan("ru.sber.services")
 class AnotherServicesConfig
