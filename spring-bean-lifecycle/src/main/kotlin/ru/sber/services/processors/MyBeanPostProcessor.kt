8b818b68ce8936f363564b16dc81ab0e9dfa7322
heckfy88 <54860448+heckfy88@users.noreply.github.com>
2021-10-19 22:47:12 +0300

    Spring2_v2

--- spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanPostProcessor.kt
+++ spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanPostProcessor.kt
@@ -8,10 +8,14 @@ import org.springframework.stereotype.Component
 class MyBeanPostProcessor : BeanPostProcessor {
 
     override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any? {
+        if (bean is CombinedBean)
+            bean.postProcessBeforeInitializationOrderMessage = "postProcessBeforeInitialization() is called"
         return bean
     }
 
     override fun postProcessAfterInitialization(bean: Any, beanName: String): Any? {
+        if (bean is CombinedBean)
+            bean.postProcessAfterInitializationOrderMessage = "postProcessAfterInitialization() is called"
         return bean
     }
 
